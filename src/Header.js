e13ef981bade72f8d284d2587a1e71596b134bb3
Gary Wong <91444301+HKC0T@users.noreply.github.com>
2023-08-19 19:29:15 +0100

    "headbar"

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <nav className="navbar">
+        <a href="/" className="home link">
+          <h1>✈️TravOrg</h1>
+        </a>
+        <ul></ul>
+      </nav>
+    </>
+  );
+}
